cb43929a2e83942ece7baae9181bcac5bf8a64ec
Giancarlo Rhodes <giancarlo.rhodes@onshoreoutsourcing.com>
2021-11-19 11:21:30 -0600

    working on ispicked

--- Bingo/MasterCard.cs
+++ Bingo/MasterCard.cs
@@ -10,6 +10,8 @@ namespace Bingo
     {
         // properties
         public CardSpot[,] Card = new CardSpot[5, 15];
+        //public List<CardSpot> List = new List<CardSpot>(); // could have used this instead of array
+
        
         // constructors
         public MasterCard() 
@@ -35,6 +37,60 @@ namespace Bingo
             }             
         }
 
+        public int Pick() 
+        {
+            // get a random number from 1 to  75
+            // 75 =  [4, 14]
+
+            Random _random = new Random();
+            int _number = _random.Next((int)CardRange.min, (int)CardRange.max);
+
+            // if that number already picked? if so, pick again
+            while (IsPicked(_number))
+            {
+                _number = _random.Next((int)CardRange.min, (int)CardRange.max);
+            }
+
+            // keep picking randomly until I get a number
+
+            // update the CardSpot array
+
+            return 0;
+        }
+
+
+        private bool IsPicked(int inNumber) 
+        {
+            bool _return = false;
+            for (int letters = 0; letters < 5; letters++) // B  I  N  G O
+            {
+                for (int numbers = 0; numbers < 15; numbers++) // 1 thru 15
+                {
+                    if (this.Card[letters, numbers].Number == inNumber && this.Card[letters, numbers].IsPicked == false)
+                    {
+                        _return = false;
+                        break;
+                    }
+                    else if (this.Card[letters, numbers].Number == inNumber && this.Card[letters, numbers].IsPicked == true)
+                    {
+                        _return = true;
+                        break;
+                    }
+                }               
+            }
+
+            return _return;
+        }
+
+    }
+
+
+
+    enum CardRange 
+    { 
+        min = 1,
+        max = 75
+    
     }
 
 
